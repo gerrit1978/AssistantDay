bf94b8215e7a81ccc16e9468618c9b769762b74a
Gerrit Vos <gevo27@gmail.com>
2013-03-25 09:27:52 +0100

    Finishing preview

--- sites/all/themes/hedgecomm/scripts/scripts.js
+++ sites/all/themes/hedgecomm/scripts/scripts.js
@@ -47,4 +47,45 @@ jQuery(document).ready(function($) {
   }
 */
 
+  $('a.fancybox').click(function(e) {
+		$.fancybox({
+		  'helpers' : {
+		    'overlay' : {
+		      'css' : {
+		        'background' : 'rgba(10, 10, 10, 0.85)'
+		      }
+		    }
+		  },
+	    'padding'        : 0,
+	    'autoScale'      : false,
+	    'transitionIn'   : 'none',
+	    'transitionOut'  : 'none',
+	    'title'          : this.title,
+	    'width'          : 680,
+	    'height'         : 495,
+	    'href'           : "http://www.youtube.com/v/Pb1XXs7e7ac&fs=1&autoplay=1",
+	    'type'           : 'swf',
+	    'swf'            : {
+	        'wmode'              : 'transparent',
+	        'allowfullscreen'    : 'true'
+	    }
+    });
+  
+  });
+
+  $('#webform-client-form-7 #webform-component-gift-markup ul li').click(function() {
+    
+    // change value of select options for form
+    var index = $(this).index();
+    var indexPlusOne = index + 1;
+    var selector = "#webform-component-gift #edit-submitted-gift option:eq(" + indexPlusOne + ")";
+    $(selector).attr('selected', 'selected');
+    
+    // make current gift selected
+    $('ul.gifts li').removeClass('selected');
+    $(this).addClass('selected');
+    
+  });
+
+
 });
